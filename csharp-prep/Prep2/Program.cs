d907ea58cf0ca45a2c8536d8573d3357c7303b06
BrendanB222 <bar24079@byui.edu>
2025-09-20 16:13:52 -0500

    Wrote the program for the first prep activity.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letterGrade = "";
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (grade % 10 >= 7 && letterGrade != "A" && letterGrade != "F")
+        {
+            letterGrade += "+";
+        }
+        else if (grade % 10 < 3 && letterGrade != "F")
+        {
+            letterGrade += "-";
+        }
+
+        Console.WriteLine($"You earned a grade {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Better luck next time.");
+        }
     }
 }
