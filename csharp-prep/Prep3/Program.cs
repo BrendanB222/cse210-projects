0324379f66e149c1a18d814d70ad689a972d8b37
BrendanB222 <bar24079@byui.edu>
2025-09-25 15:04:41 -0500

    Wrote the program for the third prep activity.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = "yes";
+
+        while (response == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            Console.WriteLine("A magic number has been chosen.");
+            int guessCount = 0;
+            int guess = 0;
+
+            while (guess != number)
+            {
+                guessCount++;
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"You guessed {guessCount} times.");
+            Console.Write("Do you want to play again? ");
+            response = Console.ReadLine();
+        }
     }
 }
