3c9b2396955a003271fb9607802189aef20db11b
BrendanB222 <bar24079@byui.edu>
2025-11-13 10:02:34 -0600

    Completed Learning Activity 05.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("red", 2.5);
+        Rectangle rectangle = new Rectangle("green", 1.2, 3.2);
+        Circle circle = new Circle("blue", 2.0);
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
